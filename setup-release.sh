f4a50f4e1e1471b50fa9eb666396a738ac91b285
Dan Paulat <dpaulat@gmail.com>
2023-04-21 13:44:46 -0500

    Updating scripts and build files to reference Qt 6.5, and update OpenSSL to 3.1.0

--- setup-release.sh
+++ setup-release.sh
@@ -2,4 +2,4 @@
 ./tools/setup-common.sh
 mkdir -p build-release
 cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_PREFIX_PATH=/opt/Qt/6.4.2/gcc_64 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_PREFIX_PATH=/opt/Qt/6.5.0/gcc_64 ..
