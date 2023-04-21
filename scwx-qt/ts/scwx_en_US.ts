a83065acd8678aff4c27f90c5bf65e7b0b50cb69
Dan Paulat <dpaulat@gmail.com>
2023-04-21 13:45:00 -0500

    Adding recent translation updates

--- scwx-qt/ts/scwx_en_US.ts
+++ scwx-qt/ts/scwx_en_US.ts
@@ -2,11 +2,142 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US" sourcelanguage="en_US">
 <context>
-    <name>MainWindow</name>
+    <name>AboutDialog</name>
     <message>
-        <source>Toolbox</source>
+        <source>About Supercell Wx</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Supercell Wx</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version X.Y.Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Git Revision 0000000000</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright © 2021-YYYY Dan Paulat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlertDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>First</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source># of #</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlertDockWidget</name>
+    <message>
+        <source>Alerts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Go</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Active Alerts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlertModel</name>
+    <message>
+        <source>ETN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Office ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Phenomenon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Significance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Counties / Areas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>End Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImGuiDebugDialog</name>
+    <message>
+        <source>ImGui Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Context</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
     <message>
         <source>...</source>
         <translation type="unfinished"></translation>
@@ -60,11 +191,199 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>&amp;Open...</source>
+        <source>&amp;Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Ctrl+O</source>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Radar Toolbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resource Explorer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Expand All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Collapse All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Resource Explorer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;NEXRAD Product...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text &amp;Event Product...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Alerts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;ImGui Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;User Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Discord</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;GitHub Repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dump Radar &amp;Product Records</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Product</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 of %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Phenomenon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inactive</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RadarSiteDialog</name>
+    <message>
+        <source>Select Radar Site</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RadarSiteModel</name>
+    <message>
+        <source>Site ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Place</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Palettes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Radar Site</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grid Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mapbox API Key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font Sizes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grid Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Debug Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Color Tables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alerts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -72,6 +391,33 @@
     <name>scwx::qt::main::MainWindow</name>
     <message>
         <source>Unrecognized NEXRAD Product:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Radar &amp;Toolbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Resource Explorer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Alerts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>scwx::qt::ui::AboutDialog</name>
+    <message>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Git Revision &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright © 2021-%1 Dan Paulat</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -87,6 +433,13 @@
     </message>
     <message>
         <source>Declutter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>scwx::qt::ui::Level3ProductsWidgetImpl</name>
+    <message>
+        <source>Tilt %1</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
