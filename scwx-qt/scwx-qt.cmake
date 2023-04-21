a264bd6b1b8427813c9c0c195a9f7d59cf854b2f
Dan Paulat <dpaulat@gmail.com>
2023-04-21 18:06:57 -0500

    Set Qt NO_TRANSLATIONS (QTBUG-112204)

--- scwx-qt/scwx-qt.cmake
+++ scwx-qt/scwx-qt.cmake
@@ -399,12 +399,16 @@ install(TARGETS supercell-wx
           COMPONENT supercell-wx
           OPTIONAL)
 
+# NO_TRANSLATIONS is needed for Qt 6.5.0 (will be fixed in 6.5.1)
+# https://bugreports.qt.io/browse/QTBUG-112204
 qt_generate_deploy_app_script(TARGET qmaplibregl
                               FILENAME_VARIABLE deploy_script_qmaplibregl
+                              NO_TRANSLATIONS
                               NO_UNSUPPORTED_PLATFORM_ERROR)
 
 qt_generate_deploy_app_script(TARGET supercell-wx
                               FILENAME_VARIABLE deploy_script_scwx
+                              NO_TRANSLATIONS
                               NO_UNSUPPORTED_PLATFORM_ERROR)
 
 install(SCRIPT ${deploy_script_qmaplibregl}
