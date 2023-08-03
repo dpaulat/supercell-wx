f9fc32e479236bc93cff3dbaa3d15405d6e846b8
Dan Paulat <dpaulat@gmail.com>
2023-08-03 06:34:19 -0500

    Ignore Linux stb_image warning

--- scwx-qt/source/scwx/qt/external/stb_image.cpp
+++ scwx-qt/source/scwx/qt/external/stb_image.cpp
@@ -1,4 +1,14 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ASSERT(x)
 #define STBI_FAILURE_USERMSG
+
+#if defined(__GNUC__)
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+
 #include <stb_image.h>
+
+#if defined(__GNUC__)
+#   pragma GCC diagnostic pop
+#endif
