c8aacd688a624d81278e2d01b7eafc6abb00131e
Dan Paulat <dpaulat@gmail.com>
2023-09-13 22:25:29 -0500

    Update Qt to 6.5.2

--- setup-release.bat
+++ setup-release.bat
@@ -1,5 +1,5 @@
 call tools\setup-common.bat
 mkdir build-release
 cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_PREFIX_PATH=C:/Qt/6.5.0/msvc2019_64 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_PREFIX_PATH=C:/Qt/6.5.2/msvc2019_64 ..
 pause
