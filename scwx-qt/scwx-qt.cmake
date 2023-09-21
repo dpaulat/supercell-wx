0d0bdd4f8b52f4404247368bc1cdb4d7157c82ca
Dan Paulat <dpaulat@gmail.com>
2023-09-20 23:49:38 -0500

    Updating zone and county files for 19 September 2023 release
    Closes #64

--- scwx-qt/scwx-qt.cmake
+++ scwx-qt/scwx-qt.cmake
@@ -235,11 +235,11 @@ set(JSON_FILES res/config/radar_sites.json)
 
 set(TS_FILES ts/scwx_en_US.ts)
 
-set(COUNTY_DBF_FILES ${SCWX_DIR}/data/db/c_08mr23.dbf)
-set(ZONE_DBF_FILES   ${SCWX_DIR}/data/db/fz08mr23.dbf
-                     ${SCWX_DIR}/data/db/mz08mr23.dbf
+set(COUNTY_DBF_FILES ${SCWX_DIR}/data/db/c_19se23.dbf)
+set(ZONE_DBF_FILES   ${SCWX_DIR}/data/db/fz19se23.dbf
+                     ${SCWX_DIR}/data/db/mz19se23.dbf
                      ${SCWX_DIR}/data/db/oz08mr23.dbf
-                     ${SCWX_DIR}/data/db/z_08mr23.dbf)
+                     ${SCWX_DIR}/data/db/z_19se23.dbf)
 set(COUNTIES_SQLITE_DB ${scwx-qt_BINARY_DIR}/res/db/counties.db)
 
 set(VERSIONS_INPUT  ${scwx-qt_SOURCE_DIR}/source/scwx/qt/main/versions.hpp.in)
