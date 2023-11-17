92800a2d36bbc9cacd9daa0a3581291580623903
Dan Paulat <dpaulat@gmail.com>
2023-11-17 05:49:23 -0600

    Update dependency Qt to 6.6.0

--- setup-debug.sh
+++ setup-debug.sh
@@ -2,4 +2,4 @@
 ./tools/setup-common.sh
 mkdir -p build-debug
 cd build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_PREFIX_PATH=/opt/Qt/6.5.3/gcc_64 ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_PREFIX_PATH=/opt/Qt/6.6.0/gcc_64 ..
