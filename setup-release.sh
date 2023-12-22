7d5fe523f30699e7dbe1ed8db003dd241d6d1bd5
Dan Paulat <dpaulat@gmail.com>
2023-12-22 13:53:27 -0600

    Additional Conan 2.x, multi-config and CI updates

--- setup-release.sh
+++ setup-release.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 ./tools/setup-common.sh
+
 build_dir=build-release
 build_type=Release
 qt_version=6.6.1
 script_dir="$(dirname "$(readlink -f "$0")")"
+
 mkdir -p ${build_dir}
-cmake -B ${build_dir} -S . -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_CONFIGURATION_TYPES=${build_type} -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=${script_dir}/external/cmake-conan/conan_provider.cmake -DCMAKE_PREFIX_PATH=/opt/Qt/${qt_version}/gcc_64 -G Ninja
+cmake -B ${build_dir} -S . \
+	-DCMAKE_BUILD_TYPE=${build_type} \
+	-DCMAKE_CONFIGURATION_TYPES=${build_type} \
+	-DCMAKE_INSTALL_PREFIX=${build_dir}/${build_type}/supercell-wx \
+	-DCMAKE_PREFIX_PATH=/opt/Qt/${qt_version}/gcc_64 \
+	-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=${script_dir}/external/cmake-conan/conan_provider.cmake \
+	-G Ninja
