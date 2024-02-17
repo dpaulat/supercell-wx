fb06ce750583baa5db76124ffb003f5f761c9958
Dan Paulat <dpaulat@gmail.com>
2024-02-17 00:10:00 -0600

    Include OverlayProductView in MapContext

--- scwx-qt/source/scwx/qt/map/map_context.hpp
+++ scwx-qt/source/scwx/qt/map/map_context.hpp
@@ -12,6 +12,7 @@ namespace qt
 namespace view
 {
 
+class OverlayProductView;
 class RadarProductView;
 
 } // namespace view
@@ -34,19 +35,22 @@ public:
    MapContext(MapContext&&) noexcept;
    MapContext& operator=(MapContext&&) noexcept;
 
-   std::weak_ptr<QMapLibreGL::Map>          map() const;
-   MapSettings&                             settings();
-   float                                    pixel_ratio() const;
-   std::shared_ptr<view::RadarProductView>  radar_product_view() const;
-   common::RadarProductGroup                radar_product_group() const;
-   std::string                              radar_product() const;
-   int16_t                                  radar_product_code() const;
-   QMapLibreGL::CustomLayerRenderParameters render_parameters() const;
+   std::weak_ptr<QMapLibreGL::Map>           map() const;
+   MapSettings&                              settings();
+   float                                     pixel_ratio() const;
+   std::shared_ptr<view::OverlayProductView> overlay_product_view() const;
+   std::shared_ptr<view::RadarProductView>   radar_product_view() const;
+   common::RadarProductGroup                 radar_product_group() const;
+   std::string                               radar_product() const;
+   int16_t                                   radar_product_code() const;
+   QMapLibreGL::CustomLayerRenderParameters  render_parameters() const;
 
-   void set_map(std::shared_ptr<QMapLibreGL::Map> map);
+   void set_map(const std::shared_ptr<QMapLibreGL::Map>& map);
+   void set_overlay_product_view(
+      const std::shared_ptr<view::OverlayProductView>& overlayProductView);
    void set_pixel_ratio(float pixelRatio);
    void set_radar_product_view(
-      std::shared_ptr<view::RadarProductView> radarProductView);
+      const std::shared_ptr<view::RadarProductView>& radarProductView);
    void set_radar_product_group(common::RadarProductGroup radarProductGroup);
    void set_radar_product(const std::string& radarProduct);
    void set_radar_product_code(int16_t radarProductCode);
