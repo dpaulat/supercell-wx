57a1678d23ddd0dceb44b18457933e97bc67030a
Dan Paulat <dpaulat@gmail.com>
2024-06-14 01:22:29 -0500

    DLLs should be copied from bindirs, not libdirs

--- conanfile.py
+++ conanfile.py
@@ -34,8 +34,9 @@ class SupercellWxConan(ConanFile):
 
     def generate(self):
         for dep in self.dependencies.values():
+            if dep.cpp_info.bindirs:
+                copy(self, "*.dll", dep.cpp_info.bindirs[0], self.build_folder)
             if dep.cpp_info.libdirs:
-                copy(self, "*.dll", dep.cpp_info.libdirs[0], self.build_folder)
                 copy(self, "*.dylib", dep.cpp_info.libdirs[0], self.build_folder)
 
     def build(self):
