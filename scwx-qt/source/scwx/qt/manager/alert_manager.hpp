1cdea4b1378ec9ff49df02f797b70026d2d8f2e7
AdenKoperczak <awkoperczak@gmail.com>
2024-07-02 12:31:05 -0400

    Updated alert_manager.hpp to match last commit

--- scwx-qt/source/scwx/qt/manager/alert_manager.hpp
+++ scwx-qt/source/scwx/qt/manager/alert_manager.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <scwx/qt/config/radar_site.hpp>
+
 #include <memory>
 
 #include <QObject>
@@ -20,7 +22,7 @@ public:
    explicit AlertManager();
    ~AlertManager();
 
-   void AlertManager::SetRadarSite(const std::string& radarSite);
+   void SetRadarSite(std::shared_ptr<config::RadarSite> radarSite);
    static std::shared_ptr<AlertManager> Instance();
 
 private:
