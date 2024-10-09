2f37e42b388a60e951c705d30aabe8f839ee49b6
Dan Paulat <dpaulat@gmail.com>
2024-10-09 05:54:57 -0500

    Add qt_arch variable to setup scripts, updating Windows to msvc2022

--- setup-debug.bat
+++ setup-debug.bat
@@ -3,10 +3,11 @@ call tools\setup-common.bat
 set build_dir=build-debug
 set build_type=Debug
 set qt_version=6.8.0
+set qt_arch=msvc2022_64
 
 mkdir %build_dir%
 cmake -B %build_dir% -S . ^
     -DCMAKE_BUILD_TYPE=%build_type% ^
     -DCMAKE_CONFIGURATION_TYPES=%build_type% ^
-    -DCMAKE_PREFIX_PATH=C:/Qt/%qt_version%/msvc2019_64
+    -DCMAKE_PREFIX_PATH=C:/Qt/%qt_version%/%qt_arch%
 pause
--- setup-release.sh
+++ setup-release.sh
@@ -4,6 +4,7 @@
 build_dir=${1:-build-release}
 build_type=Release
 qt_version=6.8.0
+qt_arch=gcc_64
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p ${build_dir}
@@ -11,5 +12,5 @@ cmake -B ${build_dir} -S . \
 	-DCMAKE_BUILD_TYPE=${build_type} \
 	-DCMAKE_CONFIGURATION_TYPES=${build_type} \
 	-DCMAKE_INSTALL_PREFIX=${build_dir}/${build_type}/supercell-wx \
-	-DCMAKE_PREFIX_PATH=/opt/Qt/${qt_version}/gcc_64 \
+	-DCMAKE_PREFIX_PATH=/opt/Qt/${qt_version}/${qt_arch} \
 	-G Ninja
