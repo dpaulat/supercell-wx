708ee22bef404d143489692395ad269327df9208
TornadoRTX <143273950+TornadoRTX@users.noreply.github.com>
2025-04-25 21:20:57 -0400

    Update main_window.hpp (#1)

--- scwx-qt/source/scwx/qt/main/main_window.hpp
+++ scwx-qt/source/scwx/qt/main/main_window.hpp
@@ -1,68 +1,40 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPropertyAnimation>
 
-QT_BEGIN_NAMESPACE
-namespace Ui
-{
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-namespace scwx
-{
-namespace qt
-{
-namespace main
-{
-
-class MainWindowImpl;
+namespace scwx {
+namespace qt {
+namespace main {
 
 class MainWindow : public QMainWindow
 {
    Q_OBJECT
 
 public:
-   MainWindow(QWidget* parent = nullptr);
+   explicit MainWindow(QWidget* parent = nullptr);
    ~MainWindow();
 
-   void keyPressEvent(QKeyEvent* ev) override final;
-   void keyReleaseEvent(QKeyEvent* ev) override final;
-   void showEvent(QShowEvent* event) override;
-   void closeEvent(QCloseEvent* event) override;
-
-signals:
-   void ActiveMapMoved(double latitude, double longitude);
-
 private slots:
-   void on_actionOpenNexrad_triggered();
-   void on_actionOpenTextEvent_triggered();
-   void on_actionSettings_triggered();
-   void on_actionExit_triggered();
-   void on_actionGpsInfo_triggered();
-   void on_actionColorTable_triggered(bool checked);
-   void on_actionRadarRange_triggered(bool checked);
-   void on_actionRadarSites_triggered(bool checked);
-   void on_actionPlacefileManager_triggered();
-   void on_actionMarkerManager_triggered();
-   void on_actionLayerManager_triggered();
-   void on_actionImGuiDebug_triggered();
-   void on_actionDumpLayerList_triggered();
-   void on_actionDumpRadarProductRecords_triggered();
-   void on_actionRadarWireframe_triggered(bool checked);
-   void on_actionUserManual_triggered();
-   void on_actionDiscord_triggered();
-   void on_actionGitHubRepository_triggered();
-   void on_actionCheckForUpdates_triggered();
-   void on_actionAboutSupercellWx_triggered();
-   void on_radarSiteHomeButton_clicked();
-   void on_radarSiteSelectButton_clicked();
+   void ToggleRadarProductPanel();
 
 private:
-   std::unique_ptr<MainWindowImpl> p;
-   Ui::MainWindow*                 ui;
+   void SetupUi();
+   void SetupRadarProductPanel();
 
-   friend class MainWindowImpl;
+   // UI Elements
+   QWidget* centralWidget_ {nullptr};
+   QVBoxLayout* mainLayout_ {nullptr};
+
+   QPushButton* productToggleButton_ {nullptr};
+   QWidget* radarProductPanel_ {nullptr};
+   QVBoxLayout* radarProductLayout_ {nullptr};
+
+   QPropertyAnimation* slideAnimation_ {nullptr};
+   bool isProductPanelVisible_ {true};
 };
 
 } // namespace main
