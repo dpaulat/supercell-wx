65c912197f1eee5c1ee75c0aad7bddd0ac4b5ae9
AdenKoperczak <awkoperczak@gmail.com>
2025-05-25 12:38:13 -0400

    Add docs to derived product code

--- wxdata/include/scwx/derivers/base_deriver.hpp
+++ wxdata/include/scwx/derivers/base_deriver.hpp
@@ -23,19 +23,77 @@ public:
    BaseDeriver& operator=(const BaseDeriver&) = delete;
    BaseDeriver& operator=(BaseDeriver&&)      = delete;
 
-   virtual bool needs_level_2_input() = 0;
-   virtual const std::unordered_set<std::string>&
-                                              get_level_3_input_products() = 0;
-   virtual std::shared_ptr<data::DerivedData> get_output()                 = 0;
+
+   /**
+    * Feed the deriver a new level 2 file.
+    *
+    * @param file The level 2 file to update the deriver with
+    */
    void set_level_2_input_file(std::shared_ptr<wsr88d::Ar2vFile> file);
+
+   /**
+    * Feed the deriver a new level 3 file for a specific product.
+    *
+    * @param product The name of the product to feed the file
+    * @param file The level 3 file to update the deriver with
+    */
    void set_level_3_input_file(const std::string&                  product,
                                std::shared_ptr<wsr88d::Level3File> file);
 
+   /**
+    * If the subclass derives from level 2 products.
+    *
+    * @return If the subclass uses level 2 products
+    */
+   virtual bool needs_level_2_input() = 0;
+
+   /**
+    * The level 3 products that the subclass derives from.
+    *
+    * @return The level 3 products that the subclass derives from
+    */
+   virtual const std::unordered_set<std::string>&
+                                              get_level_3_input_products() = 0;
+
+   /**
+    * Retreve the latest derived data from the subclass. Data may be updated if
+    * Inputs have been updated.
+    *
+    * @return The derived data the subclass has calculate
+    */
+   virtual std::shared_ptr<data::DerivedData> get_output()                 = 0;
+
 protected:
+   /**
+    * Allows the subclass to get the current level 2 file.
+    *
+    * @return The current level 2 file
+    */
    std::shared_ptr<wsr88d::Ar2vFile> get_level_2_file();
+
+   /**
+    * Allows the subclass to get the current level 3 file for a product.
+    *
+    * @param product The name of the product to get the file for
+    *
+    * @return The current level 3 file
+    */
    std::shared_ptr<wsr88d::Level3File>
    get_level_3_file(const std::string& product);
+
+   /**
+    * Gets if any of the files have been changed since the last time data was
+    * retrieve.
+    *
+    * @return If any of the files have been changed
+    */
    bool get_changed();
+
+   /**
+    * Sets the changed flag. Normally used to clear it by the subclass.
+    *
+    * @param changed The new value of the changed flag
+    */
    void set_changed(bool changed);
 
 private:
--- wxdata/include/scwx/derivers/data/derived_radial_data.hpp
+++ wxdata/include/scwx/derivers/data/derived_radial_data.hpp
@@ -21,18 +21,77 @@ public:
    DerivedRadialData& operator=(const DerivedRadialData&) = delete;
    DerivedRadialData& operator=(DerivedRadialData&&)      = delete;
 
+   // TODO things such as gate width, range, and elevation all need to be
+   // brough into this class
+
+   /**
+    * The number of radials that this data has
+    *
+    * @param file The number of radials that this data has
+    */
    [[nodiscard]] size_t radials() const;
+
+   /**
+    * The number of gates that this data has
+    *
+    * @param file The number of gates that this data has
+    */
    [[nodiscard]] size_t gates() const;
 
+   /**
+    * Sets the value and code for a given bin
+    *
+    * @param radial The radial number of the bin
+    * @param gate The gate number of the bin
+    * @param value The value the bin will have
+    * @param code The code the bin will have
+    */
    void set_bin(size_t                               radial,
                 size_t                               gate,
                 float                                value,
                 std::optional<wsr88d::DataLevelCode> code);
+
+   /**
+    * Sets the start and delta angle for a given radial
+    *
+    * @param radial The radial number of the bin
+    * @param startAngle The starting angle of the radial
+    * @param deltaAngle The change in the angle of the radial
+    */
    void set_radial(size_t radial, float startAngle, float deltaAngle);
 
+   /**
+    * Gets the code for a given bin
+    *
+    * @param radial The radial number of the bin
+    * @param gate The gate number of the bin
+    * @return code The code of the bin
+    */
    std::optional<wsr88d::DataLevelCode> get_code(size_t radial, size_t gate);
+
+   /**
+    * Gets the value for a given bin
+    *
+    * @param radial The radial number of the bin
+    * @param gate The gate number of the bin
+    * @return value The value of the bin
+    */
    float                                get_value(size_t radial, size_t gate);
+
+   /**
+    * Gets the start angle for the radial
+    *
+    * @param radial The radial number of the bin
+    * @return The start angle for the radial
+    */
    float get_start_angle(size_t radial);
+
+   /**
+    * Gets the delta angle for the radial
+    *
+    * @param radial The radial number of the bin
+    * @return The delta angle for the radial
+    */
    float get_delta_angle(size_t radial);
 
 private:
