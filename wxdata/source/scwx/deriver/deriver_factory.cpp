61aa953cc051e6cf97f714f4415ac19d0deed721
AdenKoperczak <awkoperczak@gmail.com>
2025-06-21 12:04:45 -0400

    make a somewhat more complete menu for selecting derived products (still WIP)

--- wxdata/source/scwx/deriver/deriver_factory.cpp
+++ wxdata/source/scwx/deriver/deriver_factory.cpp
@@ -4,6 +4,27 @@
 namespace scwx::deriver
 {
 
+static const std::vector<std::string> kDerivedProductCategories_ = {
+   "SRV",
+};
+
+static const std::unordered_map<std::string, std::vector<std::string>>
+   kDerivedProductsInCategories_ = {{"SRV", {"Average Storm Velocity"}}};
+
+static const std::unordered_map<std::string, std::vector<std::string>>
+   kDerivedTiltsInProduct_ = {{"Average Storm Velocity",
+                               {
+                                  "SRV-AVG-X",
+                                  "SRV-AVG-Y",
+                                  "SRV-AVG-Z",
+                                  "SRV-AVG-0",
+                                  "SRV-AVG-A",
+                                  "SRV-AVG-1",
+                                  "SRV-AVG-B",
+                                  "SRV-AVG-2",
+                                  "SRV-AVG-3",
+                               }}};
+
 std::shared_ptr<BaseDeriver>
 DeriverFactory::CreateDeriver(const std::string& product)
 {
@@ -25,6 +46,35 @@ DeriverFactory::GetDeriveableProducts(const std::string& product)
 
    const static std::unordered_map<std::string, DerivedProductInfo> empty = {};
    return empty;
+}
+
+const std::vector<std::string>& DeriverFactory::GetDerivedProductCategories()
+{
+   return kDerivedProductCategories_;
+}
+
+const std::vector<std::string>&
+DeriverFactory::GetDerivedProductsInCategory(const std::string& category)
+{
+   static const std::vector<std::string> empty = {};
+   const auto& productsIt = kDerivedProductsInCategories_.find(category);
+   if (productsIt == kDerivedProductsInCategories_.cend())
+   {
+      return empty;
+   }
+   return productsIt->second;
+}
+
+const std::vector<std::string>&
+DeriverFactory::GetDerivedTiltsForProducts(const std::string& category)
+{
+   static const std::vector<std::string> empty = {};
+   const auto& tiltsIt = kDerivedTiltsInProduct_.find(category);
+   if (tiltsIt == kDerivedTiltsInProduct_.cend())
+   {
+      return empty;
+   }
+   return tiltsIt->second;
 }
 
 } // namespace scwx::deriver
