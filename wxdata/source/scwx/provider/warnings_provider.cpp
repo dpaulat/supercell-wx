7f0e80811558ea37583a57ac25cb92cbf5767cdf
Dan Paulat <dpaulat@gmail.com>
2026-04-11 23:47:03 -0500

    Fix improper declaration of __cpp_lib_format causing gcc-15 errors

--- wxdata/source/scwx/provider/warnings_provider.cpp
+++ wxdata/source/scwx/provider/warnings_provider.cpp
@@ -1,13 +1,5 @@
-// Prevent redefinition of __cpp_lib_format
-#if defined(_MSC_VER)
-#   include <yvals_core.h>
-#endif
-
-// Enable chrono formatters
-#ifndef __cpp_lib_format
-// NOLINTNEXTLINE(bugprone-reserved-identifier, cppcoreguidelines-macro-usage)
-#   define __cpp_lib_format 202110L
-#endif
+// Enable chrono formatter feature test macro
+#include <version>
 
 #define LIBXML_HTML_ENABLED
 
--- wxdata/source/scwx/util/time.cpp
+++ wxdata/source/scwx/util/time.cpp
@@ -1,12 +1,5 @@
-// Prevent redefinition of __cpp_lib_format
-#if defined(_MSC_VER)
-#   include <yvals_core.h>
-#endif
-
-// Enable chrono formatters
-#ifndef __cpp_lib_format
-#   define __cpp_lib_format 202110L
-#endif
+// Enable chrono formatter feature test macro
+#include <version>
 
 #include <scwx/network/ntp_client.hpp>
 #include <scwx/util/time.hpp>
