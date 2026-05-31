cc1f86332c2ae5e62a940382349e0c5762e42ac7
f1r3w477 <9701525+F1r3w477@users.noreply.github.com>
2026-05-31 13:29:14 -0500

    style: clang-format map annotation dock widget.

--- scwx-qt/source/scwx/qt/ui/map_annotation_dock_widget.cpp
+++ scwx-qt/source/scwx/qt/ui/map_annotation_dock_widget.cpp
@@ -854,8 +854,7 @@ public:
             hostIsInStableWindow ? stableParent->window() : nullptr;
          const QWidget* const previousParent = self_->parentWidget();
          self_->hide();
-         self_->setParent(ownerWindow,
-                          Qt::Tool | Qt::FramelessWindowHint);
+         self_->setParent(ownerWindow, Qt::Tool | Qt::FramelessWindowHint);
          floating_ = true;
          expanded_ = true;
          QPoint floatPos;
